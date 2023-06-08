4e7e85ac57b8c76700f50c493dc57848a2ceaffc
Catherine Devlin <devlinch@corning.com>
2023-06-08 10:10:59 -0400

    added missing images

---
+++ pycon_colombia_2023/generate-slides.sh
@@ -0,0 +1,2 @@
+quarto render data-clarity-and-blur.qmd
+open data-clarity-and-blur.html
---
+++ pycon_colombia_2023/open.sh
@@ -0,0 +1,6 @@
+
+open https://lite.datasette.io/?url=https://github.com/catherinedevlin/talks/blob/master/pycon_colombia_2023/data/planetas.sqlite
+open https://wiki.postgresql.org/wiki/Foreign_data_wrappers
+open https://lite.datasette.io/?url=https://github.com/khipulab/open-khipu-repository/blob/master/khipu.db
+open https://github.com/catherinedevlin/rogue-sql
+
